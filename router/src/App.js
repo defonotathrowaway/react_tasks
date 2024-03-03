174ef19415d7a5913f24858c27b2dbe4721e58d7
defonotathrowaway <161821393+defonotathrowaway@users.noreply.github.com>
2024-03-04 03:44:13 +0530

    router

---
+++ router/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Products from './components/Products';
+import Services from './components/Services'
+import CustomerCare from './components/CustomerCare';
+
+function App() {
+  return (
+
+    <Routes>
+      <Route path='/' element={<Services />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/customerCare' element={<CustomerCare />} />
+    </Routes>
+  );
+}
+
+export default App;
