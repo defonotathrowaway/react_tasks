adb97a4f25088862d7979b21d15c6ebab686b427
defonotathrowaway <161821393+defonotathrowaway@users.noreply.github.com>
2024-03-04 03:37:35 +0530

    components

---
+++ components/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Index from './pages/Index';
+import Book from './pages/Book';
+import Login from './pages/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Navbar />
+      <Routes>
+        <Route index element={<Index />} />
+        <Route path="book" element={<Book />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="login" element={<Login />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
